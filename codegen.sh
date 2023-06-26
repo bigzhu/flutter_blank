cd883978d2b141dbd4478b983c8fd04143bd00ce
bigzhu <vermiliondun@gmail.com>
2023-06-26 15:26:36 +0800

    add codegen

---
+++ codegen.sh
@@ -0,0 +1,5 @@
+#! /usr/bin/zsh
+source .env
+# npm install -g get-graphql-schema
+get-graphql-schema https://jysijxgffjwavdtqcuir.hasura.ap-south-1.nhost.run/v1/graphql -h "x-hasura-admin-secret=$HASURA_GRAPHQL_ADMIN_SECRET" > lib/schema.graphql
+flutter pub run build_runner build --delete-conflicting-outputs
