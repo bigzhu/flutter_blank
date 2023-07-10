ec01a9c74b67f25fd23f89e8657fef34b34cf0aa
bigzhu <vermiliondun@gmail.com>
2023-07-10 17:35:48 +0800

    fix

--- codegen.sh
+++ codegen.sh
@@ -1,4 +1,4 @@
-#! /usr/bin/zsh
+#! zsh
 source .env
 # npm install -g get-graphql-schema
 get-graphql-schema https://jysijxgffjwavdtqcuir.hasura.ap-south-1.nhost.run/v1/graphql -h "x-hasura-admin-secret=$HASURA_GRAPHQL_ADMIN_SECRET" > lib/schema.graphql
